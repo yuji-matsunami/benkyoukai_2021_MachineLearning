bebe3856f2732f390de8a38a7e32d00060799e66
yuji-matsunami <yuji.sohuto@gmail.com>
2022-02-18 12:55:41 +0900

    update model and vectorizer

--- main.py
+++ main.py
@@ -4,10 +4,11 @@ import glob
 import MeCab
 import numpy as np
 from typing import Tuple, List, Any
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 def read_file(data_path:str) -> Tuple[List[str], List[int]]:
     dir_names = [d for d in os.listdir(data_path) if os.path.isdir(os.path.join(data_path, d))] # ディレクトリのみを取得
@@ -34,8 +35,11 @@ def mecab_process(text:List[str]) -> List[str]:
         output_list.append(wakati)
     return output_list
     
-def exchange_vector(text:List[str]) -> Any:
-    vectorizer = CountVectorizer()
+def exchange_vector(text:List[str], method:str) -> Any:
+    if method == "count":
+        vectorizer = CountVectorizer()
+    elif method == "tfidf":
+        vectorizer = TfidfVectorizer()
     return vectorizer.fit_transform(text).toarray()
 
 def main():
@@ -43,19 +47,22 @@ def main():
         yml = yaml.safe_load(f)
     data_path = yml["data_path"]
     model = yml["model"]
+    method = yml["method"]
     text, label = read_file(data_path)
     text_wakati = mecab_process(text)
-    X = exchange_vector(text_wakati)
+    X = exchange_vector(text_wakati, method)
     X_train, X_test, y_train, y_test = train_test_split(X, label, test_size=0.2, random_state=1) # 教師データとテストデータに分割する
     if model == "SVC":
-        clf = SVC(C=1, kernel='rbf')
-        clf.fit(X_train, y_train)
-        score = clf.score(X_test, y_test)
-        print(f"score:{score}")
+        kernel = yml["kernel"]
+        clf = SVC(C=1, kernel=kernel)
     elif model == "logistic":
         clf = LogisticRegression()
     elif model == "RandomForest":
-        pass
+        clf = RandomForestClassifier()
+    clf.fit(X_train, y_train) # 学習
+    score = clf.score(X_test, y_test) # 評価
+    print(f"score:{score}")
+    
 
 if __name__ == "__main__":
     main()
